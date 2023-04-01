8c1bffef7e2dd203595bd9988a942b36d238deee
KariukiAntony <antonygichoya9@gmail.com>
2023-04-01 09:24:41 +0300

    creation of db

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///bankai"
+    app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+    db.init_app(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
